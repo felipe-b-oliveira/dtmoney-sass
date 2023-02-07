aa8ce5d746a0793313a7c3c57151f102cb9e6799
Felipe Oliveira <felipe.boliveira@outlook.com>
2023-02-07 14:48:28 -0300

    feat: Setup scss in Dashboard component.

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,12 +1,12 @@
-import { Summary } from "../Summary";
-import { TransactionsTable } from "../TransactionsTable";
-import { Container } from "./styles";
+import {Summary} from "../Summary";
+import {TransactionsTable} from "../TransactionsTable";
+import "./styles.scss";
 
 export function Dashboard() {
   return (
-    <Container>
+    <div className="dashboard__container">
       <Summary />
       <TransactionsTable />
-    </Container>
+    </div>
   );
 }
